f68e6ea6f97ed1d70d48cb7e383430404e53aef3
kenmiller521 <ken_miller521@yahoo.com>
2015-09-20 18:01:51 -0700

    Patrolling Monster Vision Update
    I made a variable for the line cast distance so it can be set and
    changed in the inspector. I removed some of the debugging moving code

--- Assets/Scripts/PatrollingEnemyVision.cs
+++ Assets/Scripts/PatrollingEnemyVision.cs
@@ -5,41 +5,38 @@ using UnityEngine;
 using System.Collections;
 
 public class PatrollingEnemyVision : MonoBehaviour {
-
-	//Player variable for when the linecast hits the player
-	public GameObject player; 
+	
 	//Start position for the line cast
 	private Vector2 startPos; 
 	//End position for the line cast
-	private Vector2 endPos;	  
+	private Vector2 endPos;	 
+	//Variable to set distance of the monster's vision
+	public float lineCastDistance = 0f;
 	//Tests if the monster is moving left or right
 	private bool facingRight = false;
 	// Update is called once per frame
 	void Update () 
 	{
-		//Get position of enemy dduring each frame
+		//Get position of enemy during each frame
 		Vector2 currentEnemyPos = gameObject.transform.position; 
 		//initialize the starting position every frame
 		startPos = currentEnemyPos; 
 		//initialize the end position every frame
 		endPos = currentEnemyPos; 
-		//enemy facing left change position of end of line cast to follow change of enemy position
+		//enemy facing left change position of line cast to follow change of enemy position
 		if (!facingRight) {
-			//change x position to keep the line cast a certain distance. Change this variable to increase or decrease
-			//the length of the monsters vision. THIS IS FOR THE MONSTER FACING LEFT
-			endPos.x -= 4.0f;
-			//currentEnemyPos.x -= 1.0f * Time.deltaTime; //DEBUG PURPOSES TO TEST IF LINE CAST MOVES WITH MONSTER
+			//change x position to keep the line cast a certain distance. 
+			//THIS IS FOR THE MONSTER FACING LEFT
+			endPos.x -= lineCastDistance;
 		} 
 		//enemy facing right change position of line cast to follow enemy change of position
 		else 
 		{
-			//change x position to keep the line cast a certain distance. Change this variable to increase or decrease
-			//the length of the monsters vision. THIS IS FOR THE MONSTER FACING RIGHT
-			endPos.x += 4.0f; 
-			//currentEnemyPos.x += 1.0f*Time.deltaTime;  //DEBUG PURPOSES TO TEST IF LINE CAST MOVES WITH MONSTER
+			//change x position to keep the line cast a certain distance. 
+			//THIS IS FOR THE MONSTER FACING RIGHT
+			endPos.x += lineCastDistance; 
 		}
-		//gameObject.transform.position = currentEnemyPos; //DEBUG PURPOSES TO TEST IF LINE CAST MOVES WITH MONSTER
-
+		
 		//Visually see the line cast in scene mode, NOT GAME
 		Debug.DrawLine (startPos, endPos, Color.green); 
 		//Making the line cast
@@ -47,22 +44,18 @@ public class PatrollingEnemyVision : MonoBehaviour {
 		//When the monster sees the player
 		if(EnemyVisionTrigger.collider.tag == "Player")
 		{
-			//FlipEnemy(); //DEBUG PURPOSES TO SEE IF THE LINECAST REVERSES WHEN PATROLLING MONSTER REVERSES MOVEMENT
-
-			//Code currently set to when the player collides with the line cast the monster moves toward the player faster
-			//Can be changed to what content team decides what will happen when player crosses the monster vision
-			if (!facingRight) 
-			{
-				currentEnemyPos.x -= 1.5f * Time.deltaTime;
-			} 
-			else 
-			{
-				currentEnemyPos.x += 3.5f*Time.deltaTime;
-			}
-			gameObject.transform.position = currentEnemyPos;
+			//Currently set to flip the monster to see that the line cast does 
+			//reverse when the patrolling monster flips and moves the other way. 
+			//When content team decides what would happen when the the 
+			//patrolling monster sees the player add the code in this if block
+			FlipEnemy(); 
+			
+			//CODE TO BE ADDED IN THE FUTURE
+			
 		}
 	}
-	 //Function to reverse enemy movemeny position, left or right, to test if line cast flips along with the monster
+	//Function to reverse enemy movemeny position, left or right, to 
+	//test if line cast flips along with the monster
 	void FlipEnemy(){
 		facingRight = !facingRight;
 		Vector3 theScale = transform.localScale;
