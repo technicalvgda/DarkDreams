5b023601c0dcc60175b855a541e1d40e3912edec
Sachin B. Chavan <sachin.chavan@student.csulb.edu>
2015-09-22 01:14:31 -0700

    Enables player to move left and right
    User can control horizontal movement of the player using left and right
    arrow keys as well as "a" and "d" keys.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 3f;
+	private float moveX = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveX = Input.GetAxis ("Horizontal");
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (speed * moveX, GetComponent<Rigidbody2D> ().velocity.y);
+
+	}
+}
