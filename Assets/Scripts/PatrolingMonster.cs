d24b77bfd102f7088d8138ff821f816321cb75b5
Darkchicken <joshshucker@hotmail.com>
2015-09-22 12:10:59 -0700

    Condensed Patrolling Monster Code
    Combined patrolling movement code and patrolling vision code

--- Assets/Scripts/PatrolingMonster.cs
+++ Assets/Scripts/PatrolingMonster.cs
@@ -4,52 +4,93 @@ using System.Collections;
 public class PatrolingMonster : MonoBehaviour {
 
     Vector3 startPos;
-    public bool facingRight = false;
+    private bool facingRight = true;
     public float movement;
     public float speed = 2.0f;
     public float counter = 0;
     public float distance;
     public int patrolDistance = 5;
 
+    //Start position for the line cast
+    private Vector2 startCast;
+    //End position for the line cast
+    private Vector2 endCast;
+    //Variable to set distance of the monster's vision
+    public float lineCastDistance = 0f;
 
-	// Use this for initialization
-	void Start () { 
+
+    // Use this for initialization
+    void Start () { 
 	    startPos = gameObject.transform.position;
     }
 
 	// Update is called once per frame
 	void Update () {
-        Vector3 currentPos = gameObject.transform.position;
-
+        Vector2 currentPos = gameObject.transform.position;
+        //initialize the starting position of linecast every frame
+        startCast = currentPos;
+        //initialize the end position of linecast every frame
+        endCast = currentPos;
         distance = currentPos.x - startPos.x;
 
         counter *= Time.deltaTime;
-
+       
+        //enemy facing left change position of line cast to follow change of enemy position
         if (!facingRight)
         {
             movement = speed * Time.deltaTime;
             transform.Translate(-movement, 0, 0);
+            //change x position to keep the line cast a certain distance. 
+            //THIS IS FOR THE MONSTER FACING LEFT
+            endCast.x -= lineCastDistance;
         }
-
-        else {
+        //enemy facing right change position of line cast to follow enemy change of position
+        else
+        {
             movement = speed * Time.deltaTime;
             transform.Translate(movement, 0, 0);
+            //change x position to keep the line cast a certain distance. 
+            //THIS IS FOR THE MONSTER FACING RIGHT
+            endCast.x += lineCastDistance;
         }
 
-        Debug.Log(distance);
+        //Debug.Log(distance);
 
-        if (distance > patrolDistance)
+        if (distance > patrolDistance || distance < -patrolDistance)
         {
             distance = 0;
-            facingRight = false;
+            //facingRight = !facingRight;
+            FlipEnemy();
             startPos = gameObject.transform.position;
+        }
+        //Visually see the line cast in scene mode, NOT GAME
+        Debug.DrawLine(startCast, endCast, Color.green);
+        //Making the line cast
+        RaycastHit2D EnemyVisionTrigger = Physics2D.Linecast(endCast, startCast);
+        //check if the collider exists and if the collider is the player
+        if (EnemyVisionTrigger.collider && EnemyVisionTrigger.collider.tag == "Player")
+        {
+            //Currently set to flip the monster to see that the line cast does 
+            //reverse when the patrolling monster flips and moves the other way. 
+            //When content team decides what would happen when the the 
+            //patrolling monster sees the player add the code in this if block
+
+            //FlipEnemy();
+            Debug.Log("You have been seen!");
+            //CODE TO BE ADDED IN THE FUTURE
+
         }
 
-        else if (distance < -patrolDistance) {
-            distance = 0;
-            facingRight = true;
-            startPos = gameObject.transform.position;
-        }
-	}
+
+    }
+    //Function to reverse enemy movemeny position, left or right, to 
+    //test if line cast flips along with the monster
+    void FlipEnemy()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 
 }
---
+++ Assets/Scripts/StationaryMonster.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+
+public class StationaryMonster : MonoBehaviour
+{
+    //tests whether enemy is looking for you
+    public bool enemyActive = false;
+    //tests whether enemy has seen you
+    public bool canSee = false;
+    private float seconds;
+    private float rand;
+    public Transform sightStart, sightEnd; // marks the two points of the line-of-sight
+  
+    void Start()
+    {
+        seconds = 10;             //Starts the timer at 10 seconds
+        rand = Random.value* 10; //Generate a random number from 0 to 10 
+    }
+    void Update()
+    {
+        //if you are in the enemy's field of view & the enemy is active, you lose
+        if(canSee && enemyActive)
+        {
+            Debug.Log("You lose");
+        }
+        // Draws the line so that you can see it in the scene and adjust the two points
+        Debug.DrawLine(sightStart.position, sightEnd.position, Color.cyan);
+        //sets the monsters vision to true if the player crosses its vision
+        // triggers boolean to true when the line crosses the player's layer and not the monster's own layer
+        canSee = Physics2D.Linecast(sightStart.position, sightEnd.position, 1 << LayerMask.NameToLayer("Player"));
+
+        /////////timer code for enemy
+        //door will open for 1 second within a random interval of 10 seconds
+        if (seconds <= 0)     //When the timer drops to zero, it will reset back to 10 seconds
+        {
+            seconds = 10;
+        }
+        else
+        {
+            seconds -= Time.deltaTime;
+
+            //When timer is within a certain range of the random number, it will turn the timer on
+            if ((seconds<rand + 1 && seconds> rand - 1))
+            {
+                Debug.Log("Monster Timer is on!");
+                enemyActive = true;
+            }
+            else
+            {
+                //Else it will remain off
+                enemyActive = false;
+            }
+            //Prints the timer
+           //Debug.Log("Current time is: " + seconds);
+        }
+    }
+  
+}
