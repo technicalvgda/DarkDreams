2ef50f915194da3a857d2890f1d4c4a45d97fe3c
Darkchicken <joshshucker@hotmail.com>
2015-09-22 14:20:01 -0700

    Prefabs created
    Created prefabs of important objects
    Optimized linecast for stationary enemy

--- Assets/Scripts/StationaryMonster.cs
+++ Assets/Scripts/StationaryMonster.cs
@@ -10,12 +10,23 @@ public class StationaryMonster : MonoBehaviour
     public bool canSee = false;
     private float seconds;
     private float rand;
-    public Transform sightStart, sightEnd; // marks the two points of the line-of-sight
-  
+    
+    //Start position for the line cast
+    private Vector2 startCast;
+    //End position for the line cast
+    private Vector2 endCast;
+    //Variable to set distance of the monster's vision
+    public float lineCastDistance = 0f;
     void Start()
     {
         seconds = 10;             //Starts the timer at 10 seconds
         rand = Random.value* 10; //Generate a random number from 0 to 10 
+        Vector2 currentPos = gameObject.transform.position;
+        //initialize the starting position of linecast every frame
+        startCast = currentPos;
+        //initialize the end position of linecast every frame
+        endCast = currentPos;
+        endCast.y -= lineCastDistance;
     }
     void Update()
     {
@@ -25,10 +36,10 @@ public class StationaryMonster : MonoBehaviour
             Debug.Log("You lose");
         }
         // Draws the line so that you can see it in the scene and adjust the two points
-        Debug.DrawLine(sightStart.position, sightEnd.position, Color.cyan);
+        Debug.DrawLine(startCast, endCast, Color.cyan);
         //sets the monsters vision to true if the player crosses its vision
         // triggers boolean to true when the line crosses the player's layer and not the monster's own layer
-        canSee = Physics2D.Linecast(sightStart.position, sightEnd.position, 1 << LayerMask.NameToLayer("Player"));
+        canSee = Physics2D.Linecast(endCast, startCast);
 
         /////////timer code for enemy
         //door will open for 1 second within a random interval of 10 seconds
