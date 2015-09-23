f3eb9acf2bd0899e8a423ac0b78162ba60371947
Sachin B. Chavan <sachin.chavan@student.csulb.edu>
2015-09-23 03:37:10 -0700

    Player movement pc + game over canvas
    Allows user movement left and right.
    On collision with an enemy, stops the player and activates a game over
    canvas to tell the user that game is over.

---
+++ Assets/Scripts/Player1.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1 : MonoBehaviour {
+
+	public float speed = 6f;
+	private float moveX = 0f;
+	public GameObject gameOverPanel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveX = Input.GetAxis ("Horizontal");
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (speed * moveX, GetComponent <Rigidbody2D> ().velocity.y);
+
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.name == "Enemy") {
+
+			gameOverPanel.SetActive(true);
+			speed = 0f;
+		}
+	}
+}
