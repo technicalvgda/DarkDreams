5451419510324919a63372d7ae794f595fd8d279
Lazer7 <iamjimmyjimmy@yahoo.com>
2015-09-26 13:16:56 -0700

    Player Pick Up Mechanics
    This code is to pick up any GAMEOBJECT labeled "ITEM" you can change the
    label to whatever the object is going to be in the future. Since I don't
    know what we are going to be picking up there is no effect in picking up
    anything yet. In addition the ITEM object will be destroyed on contact
    with JUST THE PLAYER. Please insert this code in the player tab.

---
+++ Assets/Scripts/Player/PlayerPickMechanics.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPickMechanics : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+    void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.name == "Item") {
+            Destroy(col.gameObject);
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
