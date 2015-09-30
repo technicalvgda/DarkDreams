a29e01b79467338500735fea51e612e6e11443b0
Nesskittycat <Nessa@dhcp-39-53-91.dhcpint.csulb.edu>
2015-09-30 16:23:12 -0700

    Adding a script
    Yay

---
+++ Assets/Scripts/ProcGen/RandomMapGeneration.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomMapGeneration : MonoBehaviour {
+
+	public Transform[] doorRoom;
+	public Transform[] regRoom;
+
+	// Use this for initialization
+	void Awake() {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
