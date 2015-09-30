b83d0eb91190a58cf7ba62103b0340f135677870
Nesskittycat <Nessa@dhcp-39-53-91.dhcpint.csulb.edu>
2015-09-30 16:49:51 -0700

    AddedSkeleton

--- Assets/Scripts/ProcGen/RandomMapGeneration.cs
+++ Assets/Scripts/ProcGen/RandomMapGeneration.cs
@@ -1,21 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
 
 public class RandomMapGeneration : MonoBehaviour {
+	public int sizeOfMapX = 1;
+	public int sizeOfMapY = 1;
 
 	public Transform[] doorRoom;
 	public Transform[] regRoom;
 
+	private int linkedDoorRoom;
+
 	// Use this for initialization
 	void Awake() {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
 	}
 
+
+	//shuffle door and reg room.
 	void initializeRooms() {
+		
+	}
+
+	//Generate randomDoorRoom
+	void generateRandomDoor() {
+
+	}
+
+	void instantiateRooms() {
+	
+
 	}
 
 }
