b68f4149ac70856d1bbe825817dcd497baf46997
Steven McCracken <stevenamccracken@gmail.com>
2015-09-30 17:31:39 -0700

    Code to shuffle array of door rooms & regular rooms

--- Assets/Scripts/ProcGen/RandomMapGeneration.cs
+++ Assets/Scripts/ProcGen/RandomMapGeneration.cs
@@ -16,11 +16,22 @@ public class RandomMapGeneration : MonoBehaviour {
 	void Awake() {
 
 	}
-
-
-	//shuffle door and reg room.
+	
+	//shuffle door and reg room
 	void initializeRooms() {
-		
+		System.Random rnd = new System.Random ();
+		for (int i = 0; i < doorRoom.Length; i++) {
+			int randomIndex = rnd.Next(doorRoom.Length); // Gets a random int from 0 up to but not including array length
+			Transform tempCopy = doorRoom[randomIndex]; // Makes copy of room at random index
+			doorRoom[randomIndex] = doorRoom[i]; // Swap part 1
+			doorRoom[i] = tempCopy; // Swap part 2
+		}
+		for (int i = 0; i < regRoom.Length; i++) { // Same but for regRoom instead of doorRoom
+			int randomIndex = rnd.Next (regRoom.Length);
+			Transform tempCopy = regRoom [randomIndex];
+			regRoom [randomIndex] = regRoom [i];
+			regRoom [i] = tempCopy;
+		}
 	}
 
 	//Generate randomDoorRoom
