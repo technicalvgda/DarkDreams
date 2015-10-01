a2c2d1dee04ce626f758de8e6ba12ed98011e3af
Nesskittycat <Nessa@Nessas-MacBook-Pro.local>
2015-09-30 19:28:10 -0700

    Added methods
    InstantiateRooms
    generateRandomDoor

--- Assets/Scripts/ProcGen/RandomMapGeneration.cs
+++ Assets/Scripts/ProcGen/RandomMapGeneration.cs
@@ -10,26 +10,66 @@ public class RandomMapGeneration : MonoBehaviour {
 	public Transform[] doorRoom;
 	public Transform[] regRoom;
 
-	private int linkedDoorRoom;
+	//create 2 linkedDoor
+	private List<int> linkedDoorRooms = new List<int>(2);
 
 	// Use this for initialization
 	void Awake() {
-
+		instantiateRooms ();
 	}
 
 
 	//shuffle door and reg room.
 	void initializeRooms() {
-		
+
 	}
 
 	//Generate randomDoorRoom
 	void generateRandomDoor() {
+		int randomDoor = 0;
+		int numberOfLinkedDoor = 0;
+		while (numberOfLinkedDoor < 2) {    
+			randomDoor = Random.Range (0, sizeOfMapX);
+			if (!linkedDoorRooms.Contains (randomDoor)) {
+				linkedDoorRooms.Add (randomDoor);
+				numberOfLinkedDoor++;
+			}
+		}
+		for (int i = 0; i < linkedDoorRooms.Count; i++) {
+			Debug.Log ("LinkedDoor: " + linkedDoorRooms[i]);
+		}
 
 	}
 
 	void instantiateRooms() {
-	
+		//level is needed to keep track of each floor.
+		int level = 0;
+		Vector3 sizeOfPrefab = new Vector3 (0,0,0);
+		for (int j = 0; j < sizeOfMapY; j++) {
+			generateRandomDoor ();
+			for (int i = 0; i < sizeOfMapX; i++) {
+				if (!linkedDoorRooms.Contains (i)) {
+					Instantiate (doorRoom [i], transform.position + sizeOfPrefab, transform.rotation);
+					sizeOfPrefab.x += doorRoom [i].localScale.x + doorRoom [i].localScale.x + 
+						doorRoom [i].localScale.x + doorRoom [i].transform.right.x;
+				} else {
+					Instantiate (regRoom [i], transform.position + sizeOfPrefab, transform.rotation);
+					sizeOfPrefab.x += doorRoom [i].localScale.x + doorRoom [i].localScale.x + 
+						doorRoom [i].localScale.x + doorRoom [i].transform.right.x;
+				}
+			}
+			linkedDoorRooms.Clear ();
+			sizeOfPrefab.x = 0;
+			sizeOfPrefab.y = 0;
+			sizeOfPrefab.z = 0;
+			level++;
+			sizeOfPrefab.y += doorRoom [j].localScale.y + doorRoom [j].localScale.y + doorRoom [j].localScale.y + 
+				doorRoom [j].transform.up.y;
+			sizeOfPrefab.y *= level;
+		}
+
+			                                                   
+
 
 	}
 
