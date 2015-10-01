733a686d47dcc75e4a7487a670d9f9824aee943c
Nesskittycat <Nessa@Nessas-MacBook-Pro.local>
2015-09-30 22:35:10 -0700

    Last Commit Before Making a Pull Request
    - Changing names to make it easier to understand the code.

--- Assets/Scripts/ProcGen/RandomMapGeneration.cs
+++ Assets/Scripts/ProcGen/RandomMapGeneration.cs
@@ -14,8 +14,8 @@ public class RandomMapGeneration : MonoBehaviour {
 	private Stack<Transform> doorRoomUnvisited = new Stack<Transform>();
 	private Stack<Transform> regRoomUnvisited = new Stack<Transform>();
 
-	//create 2 linkedDoor
-	private List<int> linkedDoorRooms = new List<int>(2);
+	//DoorRoomEachFloor
+	private List<int> doorRoomEachFloor = new List<int>(2);
 
 	private const int NUMBER_OF_DOOR_PER_FLOOR = 2;
 	// Use this for initialization
@@ -24,8 +24,7 @@ public class RandomMapGeneration : MonoBehaviour {
 		instantiateRooms ();
 
 	}
-
-
+	
 	//shuffle door and reg room.
 	void initializeRooms() {
 		//shuffle doorRooms
@@ -64,25 +63,26 @@ public class RandomMapGeneration : MonoBehaviour {
 	void generateRandomDoor() {
 		int randomDoor = 0;
 		int numberOfLinkedDoor = 0;
-		while (linkedDoorRooms.Count < 2) {    
+		while (doorRoomEachFloor.Count < 2) {    
 			randomDoor = Random.Range (0, sizeOfMapX);
-			if (!linkedDoorRooms.Contains (randomDoor)) {
-				linkedDoorRooms.Add (randomDoor);
+			if (!doorRoomEachFloor.Contains (randomDoor)) {
+				doorRoomEachFloor.Add (randomDoor);
 				numberOfLinkedDoor++;
 			}
 		}
-
 	}
 
-	void instantiateRooms() {
+	//Assume that each door and regular room has the same size.
 
+	void instantiateRooms() {
 		//level is needed to keep track of each floor.
 		int level = 0;
 		Vector3 sizeOfPrefab = new Vector3 (0,0,0);
 		for (int j = 0; j < sizeOfMapY; j++) {
 			generateRandomDoor ();
 			for (int i = 0; i < sizeOfMapX; i++) {
-				if (linkedDoorRooms.Contains (i)) {
+				if (doorRoomEachFloor.Contains (i)) {
+					//Get each room out of the stack
 					Transform doorR = doorRoomUnvisited.Pop ();
 					Instantiate (doorR, transform.position + sizeOfPrefab, transform.rotation);
 					sizeOfPrefab.x += doorRoom [i].localScale.x + doorRoom [i].localScale.x + 
@@ -95,7 +95,7 @@ public class RandomMapGeneration : MonoBehaviour {
 				}
 			}
 			//Clear the linkedDoor
-			linkedDoorRooms.Clear ();
+			doorRoomEachFloor.Clear ();
 			sizeOfPrefab.x = 0;
 			sizeOfPrefab.y = 0;
 			sizeOfPrefab.z = 0;
