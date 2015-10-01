07e8b723e57d473738ae0b5628f7b2bf47c3aa71
Doan D <tamitystg@gmail.com>
2015-10-01 05:06:43 -0700

    Title screen (prompt + 2 buttons)
    Here are what I had in the scene
    - An animated background. Just a random texture.
    - I lifted the logo from the art team's Trello
    - Some sort of menu "flow", from the Press Start prompt to the buttons
    then fade out to the test level.
    - Custom button that works via a BoxCollider2D. I'm still not familiar
    with Canvas buttons so I don't know how flexible they are, but with
    these you can dress them up however you want.
    - The buttons are animated sprites that can change states. Just trying
    to familiarize myself with the Animator.
    - Sound effects for good measure, although they're just there and not
    attached to anything for now
    
    Everything in the img and sfx folders are placeholders I cooked up
    myself so there's something to display to the screen. They need to be
    replaced by the real stuff later.
    
    script/MainTitle.cs
    This contains the code for the menu. It runs in a giant IEnumerator that
    advances depending on the user's action. It is a bit convoluted, but the
    important functions are:
    _Main(): where everything happens
    MouseCheck(), KeyCheck(): default to mouse; the key check works but is
    commented out for now
    StartGame(), Quit(): loads the scene "TestScene", exit out of the
    application
    
    script/CustomButton.cs
    Script for the 'Test' and 'Quit' button.
    
    script/fx____.cs
    Experimental scripts to make things pretty. They're not a key part of
    the title screen.
    
    Errors
    - Every once in a while a couple NullReferenceExceptions are thrown on
    the animation states. Not sure what's causing it.

---
+++ Assets/Scenes/TitleScreen_DoanD/script/FxSlide.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FxSlide : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		// hide off-screen
+		transform.position = new Vector3 (9999, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Slide(float x, float y, int direction) {
+		StartCoroutine (_Slide (x, y, direction));
+	}
+
+	// fixed values for now
+	IEnumerator _Slide(float x, float y, int direction) {
+		transform.position = new Vector3 (x, y, 0);
+
+		// 0=left-to-right, 1=right-to-left
+		if (direction == 0) {
+			for (int i = 0; i < 5; i++) {
+				transform.position += new Vector3 (0.5f, 0, 0);
+				yield return null;
+			}
+		} else {
+			for (int i = 0; i < 5; i++) {
+				transform.position -= new Vector3 (0.5f, 0, 0);
+				yield return null;
+			}
+		}
+
+		// hide again
+		transform.position = new Vector3(9999, 0, 0);
+	}
+}
