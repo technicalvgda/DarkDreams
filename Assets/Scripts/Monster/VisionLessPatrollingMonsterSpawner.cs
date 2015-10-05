055027e7032d302f1011e3fc13e21976aa95b180
ken_miller <ken_miller521@yahoo.com>
2015-10-05 02:24:29 -0700

    Vision Less Patrolling Enemy
    A monster will spawn and patrol a certain distance before despawning. I
    created a spawner prefab which turns invisible and won't react or
    trigger with the player during game time. It will spawn vision less
    patrolling enemies that will move in one direction and will kill the
    player on contact. The direction of the spawner can be set in the
    inspector and will spawn monsters going to the left or right depending
    on whether "Facing Right" is selected or not.

---
+++ Assets/Scripts/Monster/VisionLessPatrollingMonsterSpawner.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisionLessPatrollingMonsterSpawner : MonoBehaviour {
+
+	//The object that it will spawn, set in the inspector
+	public GameObject obj;
+	//Minimum time for object to spawn
+	public float spawnMin = 4f;
+	//Maximum time for object to spawn
+	public float spawnMax = 5f;
+	//Maximum amount of objects to be active at one time
+	public int maxPool = 1;
+	//Keeps track of how many are currently active
+	public int currentPool = 0;
+	//Set in inspector to make the spawner spawn objects that go left or right
+	public bool facingRight;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//Make the sprite invisible during game. Sprite will be visible so you can see where it is in Scene mode
+		GetComponent<Renderer>().material.color = Color.clear;
+		//Start the spawn
+		Spawn ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//If the current pool has reached the maximum...
+		if (currentPool >= maxPool) 		
+		{	//then stop the spawning
+			CancelInvoke();
+		}
+		else
+		{	//else keep spawning within a certain window
+			Invoke ("Spawn", Random.Range (spawnMin, spawnMax));
+		}
+	}
+	//Spawn method
+	void Spawn()
+	{
+		//Spawn the object
+		Instantiate(obj, transform.position, Quaternion.identity);
+		//Increase amount in pool by 1
+		currentPool += 1;
+	}
+}
