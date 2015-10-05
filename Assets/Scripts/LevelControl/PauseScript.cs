122ddb5c575d8d364d8e552cc23fce95cd10f034
Darkchicken <joshshucker@hotmail.com>
2015-10-05 12:41:51 -0700

    Fixed Pause Script
    Moved pause script to proper folder
    Fixed compile errors
    Removed unnecessary code

---
+++ Assets/Scripts/LevelControl/PauseScript.cs
@@ -0,0 +1,39 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class PauseScript : MonoBehaviour
+{
+
+    //public GameObject Canvas;
+    bool pause = false;
+
+    void Start()
+    {
+        //Canvas.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pause == true)
+            {
+                Time.timeScale = 1.0f;
+                //Canvas.gameObject.SetActive(false);
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+                pause = false;
+            }
+            else
+            {
+                Time.timeScale = 0.0f;
+                //Canvas.gameObject.SetActive(true);
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+                pause = true;
+            }
+        }
+    }
+   
+}
