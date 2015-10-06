b1335a0b97d0c514f3b01f85da1d9691714bd35a
Darkchicken <joshshucker@hotmail.com>
2015-10-06 13:07:10 -0700

    Camera Centered
    Added an offset value to camera so that it only shows 1 floor at a time

--- Assets/Scripts/LevelControl/CameraFollowScript.cs
+++ Assets/Scripts/LevelControl/CameraFollowScript.cs
@@ -5,7 +5,7 @@ public class CameraFollowScript : MonoBehaviour
 {
 
 	private Transform player;//our player character
-
+    public float yOffset = 7;
 	void Awake ()//on start up
 	{
         //finds the gameobject with the player tag and stores its transform component
@@ -14,7 +14,7 @@ public class CameraFollowScript : MonoBehaviour
 	void Update ()//for every frame:
 	{
         //stores a vector 2 which contains the player's x and y position
-		Vector2 cameraPosition = new Vector2(player.position.x, player.position.y);//define cameraPosition as player position
+		Vector2 cameraPosition = new Vector2(player.position.x, player.position.y + yOffset);//define cameraPosition as player position
         //sets the position of this transform (the camera) to the x and y position of the stored position, does not change z position
 		transform.position = new Vector3(cameraPosition.x, cameraPosition.y, transform.position.z);//this transform (camera game object)
 	}//position is camera position
