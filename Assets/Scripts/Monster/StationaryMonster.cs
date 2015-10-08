4df44c9a51b7ec752244717248a0c5dbe2d49e0b
Darkchicken <joshshucker@hotmail.com>
2015-10-08 11:15:16 -0700

    Fixed glitch with stationary monster
    Stationary monster would kill player when it went active if they had
    crossed its LOS at any time during play. Glitch has been fixed

--- Assets/Scripts/Monster/StationaryMonster.cs
+++ Assets/Scripts/Monster/StationaryMonster.cs
@@ -57,6 +57,10 @@ public class StationaryMonster : MonoBehaviour
             {
                 canSee = true;
             }
+            else
+            {
+                canSee = false;
+            }
         }
         else
         {
