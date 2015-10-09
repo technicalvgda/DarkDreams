2da03cd0d3c0e3ac5d581c0f2d65db94ab990d25
tamqtran <tamtran1997@gmail.com>
2015-10-08 17:37:53 -0700

    Camera Transition for Stairs
    changed the CameraFollowScript so that camera will follow the
    transition that is made by the TeleportDoors.

--- Assets/Scripts/LevelControl/CameraFollowScript.cs
+++ Assets/Scripts/LevelControl/CameraFollowScript.cs
@@ -3,19 +3,57 @@ using System.Collections;
 
 public class CameraFollowScript : MonoBehaviour
 {
-
+	
 	private Transform player;//our player character
-    public float yOffset = 7;
+	public float transitionDuration = 2.0f;
+	public Transform target;
+	public bool follow = true;
+	public float yOffset = 7;
 	void Awake ()//on start up
 	{
-        //finds the gameobject with the player tag and stores its transform component
-        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-    }
+		//finds the gameobject with the player tag and stores its transform component
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	}
 	void Update ()//for every frame:
-	{
-        //stores a vector 2 which contains the player's x and y position
-		Vector2 cameraPosition = new Vector2(player.position.x, player.position.y + yOffset);//define cameraPosition as player position
-        //sets the position of this transform (the camera) to the x and y position of the stored position, does not change z position
-		transform.position = new Vector3(cameraPosition.x, cameraPosition.y, transform.position.z);//this transform (camera game object)
+		{
+		if (follow == true) 
+		{
+			//stores a vector 2 which contains the player's x and y position
+			Vector2 cameraPosition = new Vector2 (player.position.x, player.position.y + yOffset);//define cameraPosition as player position
+			//sets the position of this transform (the camera) to the x and y position of the stored position, does not change z position
+			transform.position = new Vector3 (cameraPosition.x, cameraPosition.y, transform.position.z);//this transform (camera game object)
+		} 
+		else
+		{
+			StartCoroutine (Transition ());
+			StartCoroutine (Transition2());
+		}
 	}//position is camera position
+
+	IEnumerator Transition()
+	{
+		float t = 0.0f;
+		Vector3 startingPos = transform.position;
+		while (t < 2.5f) 
+		{
+			t += Time.deltaTime * (Time.timeScale / transitionDuration);
+			Vector3 endPos = new Vector3 (target.position.x, target.position.y, transform.position.z);
+			transform.position = Vector3.Lerp (startingPos, endPos, t);
+			yield return 0;
+		}
+	}
+	IEnumerator Transition2()
+	{
+		float t = 0.0f;
+		Vector3 endPos = transform.position;
+		while (t<2.5f)
+		{
+			t += Time.deltaTime * (Time.timeScale / transitionDuration);
+			Vector3 playerPosition = new Vector3 (player.position.x, player.position.y + yOffset, transform.position.z);
+			transform.position = Vector3.Lerp (endPos, playerPosition, t);
+			yield return 0;
+		}
+		
+	}
+	
 }
--- Assets/Scripts/LevelControl/TeleportDoors.cs
+++ Assets/Scripts/LevelControl/TeleportDoors.cs
@@ -10,37 +10,42 @@ using System.Collections;
  - Click on Door1. In the inspector tab, locate the "TeleportDoors (Script)" and find "Exit."
  - (If you can't find "Exit", be sure to check to see if the "Teleporter (Script)" is expanded)
  - Right now, it should say "None(Transform)". Click the circle on the right.
- - Look left to find the "Select Transform" window. Select Door2 object.
+ - Look left to find the "Select Transsform" window. Select Door2 object.
  - Now, select Door2, locate "TeleportDoors (Script)" and find Exit, click the circle on the right and select Door1 object.
  - Test program. Your player should be able to teleport when "Enter" or "Return" is pressed. */
 
 public class TeleportDoors : MonoBehaviour
 {
 	public Transform exit; //creates the "teleport" aspect and the Exit option in Inspector Tab.
-
     Vector2 clickPosition;
+	//reference to camera
+	CameraFollowScript cameraScript;
 
     // Use this for initialization
     void Awake()
     {
         clickPosition = new Vector2(0f, 0f);
+		cameraScript = Camera.main.GetComponent<CameraFollowScript> ();
     }
-    
     void OnTriggerStay2D ( Collider2D col)
 	{
         ///get position of click
         clickPosition.x = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
         clickPosition.y = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
         ///currently, click must be on player
-        if (col.tag == "Player" && ((Input.GetKeyDown(KeyCode.Space))||(col.OverlapPoint(clickPosition) && Input.GetMouseButtonDown(0))))
-        {
-            
-                Debug.Log("Teleport Complete!"); // confirm that teleport is complete; this can be taken out
-                TeleportToExit2D(col);
-            
-        }
+        if (col.tag == "Player" && ((Input.GetKeyDown (KeyCode.Space)) || (col.OverlapPoint (clickPosition) && Input.GetMouseButtonDown (0)))) 
+		{
+			cameraScript.follow = false;
+			cameraScript.target = exit.transform;
+			Debug.Log ("Teleport Complete!"); // confirm that teleport is complete; this can be taken out
+			TeleportToExit2D (col);
+
+		}
+		else 
+		{
+			cameraScript.follow = true;//allows smooth transition for each teleport
+		}
 	}
-	
 	void TeleportToExit2D ( Collider2D col )
 	{
 		col.transform.position = exit.transform.position; //line that teleports player
