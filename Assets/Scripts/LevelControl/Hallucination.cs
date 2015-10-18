b87ba49d1407828b4b594b825346b5d718547823
AlexTol <atol514@gmail.com>
2015-10-18 16:29:56 -0700

    The hallucination fire room
    This is the code for creating an fire room. The code script can be used
    for other hallucination rooms. The pre-fab contains one of the finished
    rooms called the fire room it's own hallucination.

---
+++ Assets/Scripts/LevelControl/Hallucination.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+//JUST DO IT!! DON'T LET YOU'RE MEME BE DREAMS
+public class Hallucination : MonoBehaviour {
+
+    bool hRoomTrigger = false;
+    //the sprite objects in the hallucination
+     public GameObject sprite1, sprite2, sprite3,sprite4,sprite5;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //sets object inactive 
+        sprite1.SetActive(false);
+        sprite2.SetActive(false);
+        sprite3.SetActive(false);
+        sprite4.SetActive(false);
+        sprite5.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+ 
+        //sets them active once person is in the room
+        if (hRoomTrigger)
+        {
+            sprite1.SetActive(true);
+            sprite2.SetActive(true);
+            sprite3.SetActive(true);
+            sprite4.SetActive(true);
+            sprite5.SetActive(true);
+
+        }
+
+        else
+        {
+            sprite1.SetActive(false);
+            sprite2.SetActive(false);
+            sprite3.SetActive(false);
+            sprite4.SetActive(false);
+            sprite5.SetActive(false);
+
+        }
+
+
+    }
+
+    //checks if person is in the room
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            hRoomTrigger = true;
+        }
+
+
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            hRoomTrigger = false;
+        }
+
+
+    }
+
+}
