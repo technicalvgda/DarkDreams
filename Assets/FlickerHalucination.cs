07dc7ad1bfeec769b5e27a6d8ccbbf35ccdaedea
Benjamin Seo <benseo000@gmail.com>
2015-10-20 19:38:27 -0700

    Hallucination Room (Flicker)
    This is one of the hallucination room where it resembles lightning.
    
    Sorry that i only did one but midterms....
    
    In the hallucination room. my goal was to emit a shadow or some thing
    horror worthy when the lightning starts. But didn't enough time.
    
    Any other question... ask me

---
+++ Assets/FlickerHalucination.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlickerHalucination : MonoBehaviour {
+    CanvasGroup canvasGroup;
+  
+    public float seconds;
+    public bool onTrigger;
+    public bool onFlicker;
+
+	// Use this for initialization
+	void Start () {
+        canvasGroup = GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 0.50f;
+        seconds = Random.Range(3,6);
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (onTrigger)
+        {
+            if (seconds <= 0)
+            {
+                seconds = Random.Range(3, 6);
+            }
+            else
+            {
+                seconds -= Time.deltaTime;
+
+                if ((seconds <= 0.30))
+                {
+                    Flash();
+                    onFlicker = true;
+                    
+                }
+                else
+                {
+                    canvasGroup.alpha = 0.50f;
+                }
+            }
+        }
+	}
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            onTrigger = true;
+        }
+    }
+
+    void Flash()
+    {
+        float alphaValue = Random.Range(0f, 0.65f);
+        canvasGroup.alpha = alphaValue;
+
+        
+
+    }
+}
