bca4581ee80a2f4f31e8027402a8ccb08284963f
Doan D <tamitystg@gmail.com>
2015-10-25 02:26:18 -0700

    Periodic pause for Chasing enemy + modified Patrolling enemy script
    Periodic pause for Chasing enemy
    
    File: ChasingMonsterB.cs
    
    Summary:
    - Rename the script and the class and drop it into the ChasingEnemy
    prefab.
    - Set the global boolean, move duration and pause duration in the
    inspector. The monster will alternate between moving and pausing on its
    path. The monster does not pause when it's chasing the player.
    - The monster chases after the player even when they walk into its line
    of sight during its pause.
    - If the player outruns it, it resumes patrolling instead of waiting out
    the remaining of its pause period.
    (I think in the original code, the monster can suddenly turn around
    mid-chase. Here you have to outrun it since it never turns around during
    a chase. Let me know if you want to keep the old behavior)
    
    Changes:
    - Added the attributes periodicPause, moveDuration and pauseDuration. If
    periodicPause is true, the object moves for moveDuration seconds then
    pauses for pauseDuration seconds.
    - Added the attribute direction. It is used to quickly "flip" any value
    that depends on the facing direction.
    - Added the attributes speedNormal and speedChasing. The monster moves
    at speedNormal speed and shifts to speedChasing when it spots the
    player.
    
    - Renamed some variables to make them clearer
    bool isCaught -> isChasing
    bool isPlayer -> isPlayerSeen
    float visionSpeedMultiplier -> speedChasing
    
    - Moved some variables into local scopes, shortened some calculations
    and checks (they should still do the same thing).
    
    - Split the huge Update() block into smaller functions:
    CheckForPlayer() checks the line cast on the player.
    Patrol() moves the monster at normal speed; makes it turn around if it
    has traveled the full distance; if periodicPause is enabled, it pauses
    for pauseDuration seconds whenever it has traveled moveDuration seconds.
    Move(float) moves the monster at a constant speed in the direction it is
    facing.
    
    ---
    Changed the PatrollingMonster script to make it work with the fixed
    Spawner
    
    File: PatrollingMonsterB.cs
    
    - Removed the reference to the spawner.
    - Added the Set(Vector3, bool) function to reinitialize the object at
    the desired position and facing direction.
    - Changed the way the traveled distance is calculated; this gets rid of
    StartPos.
    - Changed so that the object is disabled instead of destroyed when it
    reaches the maximum traveled distance.

--- Assets/NEW/PatrollingMonsterB.cs
+++ Assets/NEW/PatrollingMonsterB.cs
@@ -4,122 +4,84 @@ using System.Collections;
 
 public class PatrollingMonsterB : MonoBehaviour {
 
+	private bool facingRight = true;
 	public float speed = 2.0f;
-	public int patrolDistance = 5;
-	float accumulatedDistance;
+	public float patrolDistance = 5;
+	float accumulatedDistance = 0;
 
-	// With periodicPause on, the object moves for moveDuration second and
-	// pauses for pauseDuration seconds before moving again. Set in inspector
-	public bool periodicPause;
-	public float moveDuration;
-	public float pauseDuration;
-	WaitForSeconds pause;
-	
-	bool facingRight = true;
-
+	//to access methods and variables from the spawner
+	//private PatrollingMonsterSpawner obj;
 	PlayerControl player;
 	Transform playerPos;
 
-	// Determine how to update the object
-	IEnumerator behavior;
-	
 	// Use this for initialization
 	void Start ()
 	{
+		//Grab the spawner and get its script component
+		//obj = GameObject.Find("PatrollingEnemySpawner").GetComponent<PatrollingMonsterSpawner> ();
 		player = GameObject.FindWithTag("Player").GetComponent<PlayerControl>();
 		playerPos = player.GetComponent<Transform>();
-
-		pause = new WaitForSeconds(pauseDuration);
+		//If the spawner is facing left...
+		//if (!obj.facingRight)
+		//{   //then flip the monsters it spawns to go left
+		//	FlipEnemy();
+		//}
 	}
-	
+
 	public void Set(Vector3 position, bool facingRight)
 	{
-		// Stop the coroutine in case this method is called while it's still running
-		if (behavior != null)
-			StopCoroutine (behavior);
-
 		// Place the object at the specified position, flip its sprite if it's
 		// currently facing a different direction, reset some of its attributes
 		this.transform.position = position;
 		if (this.facingRight != facingRight)
-			Flip();
+			FlipEnemy();
 		this.facingRight = facingRight;
 		accumulatedDistance = 0;
-
+		
 		this.gameObject.SetActive (true);
-
-		// Set the type of behavior
-		if (!periodicPause)
-			behavior = _Travel();		// Move continuously
-		else
-			behavior = _TravelPause();	// Move with periodic pauses
-
-		if (behavior != null)
-			StartCoroutine(behavior);
 	}
 	
-	void Update()
-	{
-		// If this object is not on the same elevation as the player
-		if (this.transform.position.y < playerPos.position.y - 10 &&
-		    this.transform.position.y > playerPos.position.y + 10)
-		{
-			// Disable it
-			this.gameObject.SetActive(false);
-		}
-		// It won't get enabled again until the player walks across
-		// the center of a room containing the spawner. Probably
-	}
+	// Update is called once per framed
+	void Update () 
+	{	
+		//Update the enemy position every frame
+		Vector2 currentPos = gameObject.transform.position;
 
-	// Move without pausing
-	IEnumerator _Travel()
-	{
-		while (accumulatedDistance < patrolDistance)
+		//Debug.Log("ENEMY: "+ currentPos.y+ "\nPLAYER: " +playerPos.position.y); //debug purposes
+		// If the player is on our floor, run the script.
+		if (playerPos.position.y - 10 <= currentPos.y && currentPos.y <= playerPos.position.y + 10)
 		{
-			Move ();
-			yield return null;
-		}
-		this.gameObject.SetActive (false);
-	}
+			float movement = speed * Time.deltaTime;
+			if (!facingRight)
+				transform.Translate(-movement, 0, 0);
+			else
+			//Enemy facing and movement to the right
+				transform.Translate(movement, 0, 0);
+			accumulatedDistance += movement;
 
-	// Move moveDuration seconds before pausing for pauseDuration seconds
-	IEnumerator _TravelPause()
-	{
-		while (true)
-		{
-			float time = 0;
-			while (time < moveDuration)
-			{
-				Move ();
-				if (accumulatedDistance >= patrolDistance)
-				{
-					this.gameObject.SetActive (false);
-					yield break;
-				}
-				time += Time.deltaTime;
-				yield return null;
+			//If the monster has reached its target distance...	
+			if (accumulatedDistance >= patrolDistance)
+			{   //then...
+				// Disable the object
+				this.gameObject.SetActive (false);
+
+				//Decrement the spawners current pool
+				//obj.currentPool -= 1;
+				//Destroy the enemy once it reaches its target distance
+				//Destroy(gameObject);
 			}
-			yield return pause;
 		}
 	}
-
-	void Move()
+	
+	//Function to reverse enemy movemeny position, left or right, to 
+	//test if line cast flips along with the monster
+	void FlipEnemy()
 	{
-		float movement = speed * Time.deltaTime;
-		if (!facingRight)
-			transform.Translate(-movement, 0, 0);
-		else
-			transform.Translate(movement, 0, 0);
-
-		accumulatedDistance += movement;
+		//facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
 	}
-
-	void Flip()
-	{
-		transform.localScale.Scale(new Vector3(-1, 1, 1));
-		// do extra things related to flipping here
-	}
-
 	void OnTriggerEnter2D(Collider2D col)
 	{
 		//If the player collides with the patrolling enemy and not hiding
@@ -130,100 +92,3 @@ public class PatrollingMonsterB : MonoBehaviour {
 		}
 	}
 }
-
-// Old code, Ctrl+Alt+C
-
-//using UnityEngine;
-//using UnityEngine.UI;
-//using System.Collections;
-//
-//public class PatrollingMonster : MonoBehaviour {
-//	
-//	Vector3 startPos;
-//	private bool facingRight = true;
-//	float movement;
-//	public float speed = 2.0f;
-//	float distance;
-//	public int patrolDistance = 5;
-//	//to access methods and variables from the spawner
-//	private PatrollingMonsterSpawner obj;
-//	PlayerControl player;
-//	Transform playerPos;
-//	
-//	// Use this for initialization
-//	void Start ()
-//	{
-//		//Get the starting position of the enemy
-//		startPos = gameObject.transform.position;
-//		//Grab the spawner and get its script component
-//		obj = GameObject.Find("PatrollingEnemySpawner").GetComponent<PatrollingMonsterSpawner> ();
-//		player = GameObject.FindWithTag("Player").GetComponent<PlayerControl>();
-//		//If the spawner is facing left...
-//		if (!obj.facingRight)
-//		{   //then flip the monsters it spawns to go left
-//			FlipEnemy();
-//		}
-//		
-//	}
-//	
-//	// Update is called once per framed
-//	void Update () 
-//	{	
-//		
-//		//Update the enemy position every frame
-//		Vector2 currentPos = gameObject.transform.position;
-//		
-//		//Calculate the distance it has traveled
-//		distance = currentPos.x - startPos.x;
-//		//Enemy facing and movement to the left
-//		
-//		playerPos = GameObject.Find("Player").GetComponent<Transform>();
-//		//Debug.Log("ENEMY: "+ currentPos.y+ "\nPLAYER: " +playerPos.position.y); //debug purposes
-//		// If the player is on our floor, run the script.
-//		if (playerPos.position.y - 10 <= currentPos.y && currentPos.y <= playerPos.position.y + 10)
-//		{
-//			if (!facingRight)
-//			{
-//				movement = speed * Time.deltaTime;
-//				transform.Translate(-movement, 0, 0);
-//			}
-//			//Enemy facing and movement to the right
-//			else
-//			{
-//				movement = speed * Time.deltaTime;
-//				transform.Translate(movement, 0, 0);
-//			}
-//			//If the monster has reached its target distance...	
-//			if (distance > patrolDistance || distance < -patrolDistance)
-//			{   //then...
-//				//Decrement the spawners current pool
-//				obj.currentPool -= 1;
-//				//Destroy the enemy once it reaches its target distance
-//				Destroy(gameObject);
-//			}
-//		}
-//		else
-//		{
-//			//do nthing
-//		}
-//	}
-//	
-//	//Function to reverse enemy movemeny position, left or right, to 
-//	//test if line cast flips along with the monster
-//	void FlipEnemy()
-//	{
-//		facingRight = !facingRight;
-//		Vector3 theScale = transform.localScale;
-//		theScale.x *= -1;
-//		transform.localScale = theScale;
-//	}
-//	void OnTriggerEnter2D(Collider2D col)
-//	{
-//		//If the player collides with the patrolling enemy and not hiding
-//		if (col.gameObject.tag == "Player" && player.hide == false)
-//		{
-//			//Monster stops moving
-//			speed = 0;
-//		}
-//	}
-//}
