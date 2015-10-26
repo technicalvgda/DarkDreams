190ad6bd6f3283e829741ed2668c6b32eed25304
Lazer7 <iamjimmyjimmy@yahoo.com>
2015-10-26 10:26:23 -0700

    another CHange
    :dancer:

--- Assets/Scripts/Monster/DoubleDouble.cs
+++ Assets/Scripts/Monster/DoubleDouble.cs
@@ -3,30 +3,18 @@ using System.Collections;
 
 public class DoubleDouble : MonoBehaviour {
 
-    bool hRoomTrigger = false;
+    bool hRoomTrigger;
     Hallucination Room;
-    public GameObject sprite1, sprite2, sprite3, sprite4, sprite5, sprite6, sprite7, sprite8, sprite9;
 
     void Start()
     {
-        
+        Room = GetComponent<Hallucination>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (hRoomTrigger)
-        {
-        sprite1.SetActive(false);
-        sprite2.SetActive(false);
-        sprite3.SetActive(false);
-        sprite4.SetActive(false);
-        sprite5.SetActive(false);
-        sprite6.SetActive(false);
-        sprite7.SetActive(false);
-        sprite8.SetActive(false);
-        sprite9.SetActive(false);
-          }
+
         // else
         //  {
         //sprite1.SetActive(false);
@@ -37,10 +25,10 @@ public class DoubleDouble : MonoBehaviour {
     {
  
         if (col.gameObject.tag == "Player")
-        {
-            hRoomTrigger = true;
+        {      
             gameObject.SetActive(false);
-            
+            Room.hRoomTrigger = true;
+
         }
     }
 
