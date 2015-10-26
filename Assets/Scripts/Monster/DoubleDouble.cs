79d6cce1461094b08fb3037cbdcb5e20a688c39e
Lazer7 <iamjimmyjimmy@yahoo.com>
2015-10-25 17:17:01 -0700

    Double Double Hallucination Room
    We changed some variables to public in the player player control.
    Everything else is set in the Prefab. The double double code has some
    commented out sprites for later use in adding more details in the
    hallucination room. In addition, when the player collides with the
    double image then the image will disappear. We will add special affect
    next week. Brought to you by the Chao and Tol production (Alex and
    Jimmy).

---
+++ Assets/Scripts/Monster/DoubleDouble.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoubleDouble : MonoBehaviour {
+
+    bool hRoomTrigger = false;
+    //the sprite objects in the hallucination
+    //public GameObject sprite1;
+    // Use this for initialization
+    void Start()
+    {
+        //sets object inactive 
+        //sprite1.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hRoomTrigger)
+        {
+            //sprite1.SetActive(true);
+
+        }
+        else
+        {
+            //sprite1.SetActive(false);
+        }
+    }
+    //checks if person is in the room
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            gameObject.SetActive(false);
+           // hRoomTrigger = true;
+            //Debug.Log(hRoomTrigger);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Monster/DoubleHallucinationRoom.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoubleHallucinationRoom : MonoBehaviour
+{
+    public GameObject clone;
+    GameObject player;
+    PlayerControl playerScript;
+    bool facingR, facingL;
+    private bool left = false;
+    private bool right = false;
+    private float negative = -1;
+    // Use this for initialization
+    void Start()
+    {
+        facingR = false;
+        facingL = false;
+        player = GameObject.Find("Player");
+        playerScript = player.GetComponent<PlayerControl>();
+        clone.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPos = player.transform.position;
+        Quaternion playerRot = player.transform.rotation;
+
+        if (facingR)
+        {
+            clone.transform.Translate(playerScript.movement.x * negative, 0, 0);
+            if (!playerScript.facingRight && left==false)
+            {
+                Debug.Log("Facing Left");
+                clone.transform.rotation = playerRot;
+                left = true;
+                right = false;
+                negative = negative * -1;
+            }
+            else if(playerScript.facingRight && right == false)
+            {
+                Debug.Log("Facing Right");
+                playerRot.y = playerRot.y +180;
+                clone.transform.rotation = playerRot;
+                left = false;
+                right = true;
+                negative = negative * -1;
+            }
+        }
+        else if (facingL)
+        {
+            clone.transform.Translate(playerScript.movement.x * negative, 0, 0);
+            if (!playerScript.facingRight && left == false)
+            {
+                clone.transform.rotation = playerRot;
+                left = true;
+                right = false;
+                negative = negative * -1;
+            }
+            else if (playerScript.facingRight && right == false)
+            {
+                playerRot.y = playerRot.y + 180;
+                clone.transform.rotation = playerRot;
+                left = false;
+                right = true;
+                negative = negative * -1;
+            }
+        }
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Vector3 playerPos = player.transform.position;
+            Quaternion playerRot = player.transform.rotation;
+
+            clone.SetActive(true);
+
+            if (playerScript.facingRight)
+            {
+                playerPos.x = playerPos.x + 55;
+                playerRot = player.transform.rotation;
+                clone.transform.position = playerPos;
+                clone.transform.rotation = playerRot;
+                facingR = true;
+            }
+            else if (!playerScript.facingRight)
+            {
+                playerPos.x = playerPos.x - 55;
+                playerRot.y = playerRot.y + 180;
+                clone.transform.position = playerPos;
+                clone.transform.rotation = playerRot;
+                facingL = true;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            clone.SetActive(false);
+            facingR = false;
+            facingL = false;
+        }
+    }
+}
