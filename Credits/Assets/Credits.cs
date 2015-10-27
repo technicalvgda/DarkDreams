94df33633c9ec11992a65752ad1df5c0f3df6f51
dionwu110 <kevin110023@yahoo.com>
2015-10-26 22:52:15 -0700

    Added Credits
    Added a new scene for credits

---
+++ Credits/Assets/Credits.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+
+	public GameObject camera;
+	public int speed = 1;
+	public string level;
+	
+	// Update is called once per frame
+	void Update () {
+	
+		camera.transform.Translate (Vector3.down * Time.deltaTime * speed);
+	
+	}
+
+	IEnumerator waitFor(){
+
+		yield return new WaitForSeconds (20);
+		Application.LoadLevel (level);
+
+	}
+
+}
+
+
