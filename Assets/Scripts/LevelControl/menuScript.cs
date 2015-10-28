e659b9cb2a17a8575f9dd5046282768c7742e7ce
Darkchicken <joshshucker@hotmail.com>
2015-10-28 11:14:07 -0700

    Added playerpref variables
    Menu Script contains variables for music, sfk, and brightness

--- Assets/Scripts/LevelControl/menuScript.cs
+++ Assets/Scripts/LevelControl/menuScript.cs
@@ -7,12 +7,22 @@ public class menuScript : MonoBehaviour {
 	public Canvas quitMenu;
 	public Button startText;
 	public Button exitText;
-	
-	// Use this for initialization
-	void Start ()
-		
+    float brightnessLevel;
+    float musicVolume;
+    float sfxVolume;
+    // Use this for initialization
+    void Start ()
 	{
-		quitMenu = quitMenu.GetComponent<Canvas> ();
+        //Initialize variables for playerprefs (if they dont exist)
+        PlayerPrefs.SetFloat("Brightness", 0);
+        PlayerPrefs.SetFloat("Music", 0);
+        PlayerPrefs.SetFloat("SFX", 0);
+        //set variables to proper level stored in PlayerPref variables
+        brightnessLevel = PlayerPrefs.GetFloat("Brightness");
+        musicVolume = PlayerPrefs.GetFloat("Music");
+        sfxVolume = PlayerPrefs.GetFloat("SFX");
+
+        quitMenu = quitMenu.GetComponent<Canvas> ();
 		startText = startText.GetComponent<Button>();
 		exitText = exitText.GetComponent<Button>();
 		quitMenu.enabled = false;
