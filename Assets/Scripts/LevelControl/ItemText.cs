4d0ea35baca014793a28f93acf3a81b2116b5f44
Darkchicken <joshshucker@hotmail.com>
2015-10-29 11:29:52 -0700

    Fixes to attic
    Swapped attic in ending level
    Made item text appear on spacebar or click

--- Assets/Scripts/LevelControl/ItemText.cs
+++ Assets/Scripts/LevelControl/ItemText.cs
@@ -4,22 +4,33 @@ using System.Collections;
 public class ItemText : MonoBehaviour{
     PlayerControl player;
     private GameObject itemTextPanel;
-
+    bool clickedItem = false;
     void Start()
     {
         player = GameObject.Find("Player").GetComponent<PlayerControl>();
         itemTextPanel = GameObject.Find("ItemTextPanel");
         itemTextPanel.SetActive(false);
     }
-    void OnTriggerEnter2D(Collider2D other){
-        if (other.GetComponent<PlayerControl>() == null)
+    void OnTriggerStay2D(Collider2D other){
+        if (Input.GetKeyDown(KeyCode.Space) || clickedItem == true)
         {
-            return;
+            if (other.GetComponent<PlayerControl>() == null)
+            {
+                return;
+            }
+            itemTextPanel.SetActive(true);
         }
-        itemTextPanel.SetActive(true);
     }
     void OnTriggerExit2D(Collider2D other)
     {
         itemTextPanel.SetActive(false);
+    }
+    void OnMouseDown()
+    {
+        clickedItem = true;
+    }
+    void OnMouseUp()
+    {
+        clickedItem = false;
     }
 }
