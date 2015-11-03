bee1600f9b071068d70a6f34b9c8035048f61086
Darkchicken <joshshucker@hotmail.com>
2015-11-03 11:27:11 -0800

    Fixed gameover buttons
    Buttons now respond on click

--- Assets/Scripts/LevelControl/ItemText.cs
+++ Assets/Scripts/LevelControl/ItemText.cs
@@ -3,19 +3,19 @@ using UnityEngine.UI;
 using System.Collections;
 public class ItemText : MonoBehaviour
 {
-    PlayerControl player;
+    //PlayerControl player;
     private GameObject itemTextPanel;
     Vector2 clickPosition;
-    CameraFollowScript cameraScript;
+    //CameraFollowScript cameraScript;
     float clickOffsetY = 1;
     float clickOffsetX = 1;
     void Start()
     {
-        player = GameObject.Find("Player").GetComponent<PlayerControl>();
+       // player = GameObject.Find("Player").GetComponent<PlayerControl>();
         itemTextPanel = transform.Find("UICanvas/Overlay/ItemTextPanel").gameObject;//GameObject.Find("ItemTextPanel");
         itemTextPanel.SetActive(false);
         clickPosition = new Vector2(0f, 0f);
-        cameraScript = Camera.main.GetComponent<CameraFollowScript>();
+        //cameraScript = Camera.main.GetComponent<CameraFollowScript>();
     }
     void OnTriggerStay2D(Collider2D other)
     {
