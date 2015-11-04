1c394608c1c3c2005ed34832968b196b7cc04cfd
Naarith <narithchoeun@hotmail.com>
2015-11-03 17:21:46 -0800

    Item Text
    in ending level scene (attic), added item click feature to display
    itemTextPanel

--- Assets/Scripts/LevelControl/ItemText.cs
+++ Assets/Scripts/LevelControl/ItemText.cs
@@ -5,14 +5,18 @@ public class ItemText : MonoBehaviour{
     PlayerControl player;
     private GameObject itemTextPanel;
     bool clickedItem = false;
+    Vector2 clickPosition;
     void Start()
     {
         player = GameObject.Find("Player").GetComponent<PlayerControl>();
+        clickPosition = new Vector2(0f, 0f);
         itemTextPanel = transform.Find("UICanvas/Overlay/ItemTextPanel").gameObject;//GameObject.Find("ItemTextPanel");
         itemTextPanel.SetActive(false);
     }
     void OnTriggerStay2D(Collider2D other){
-        if (Input.GetKeyDown(KeyCode.Space) || clickedItem == true)
+        clickPosition.x = (Camera.main.ScreenToWorldPoint(Input.mousePosition).x);
+        clickPosition.y = (Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        if ((Input.GetKeyDown(KeyCode.Space) || clickedItem == true) || (other.OverlapPoint(clickPosition) && Input.GetMouseButtonDown(0)))
         {
             if (other.GetComponent<PlayerControl>() == null)
             {
