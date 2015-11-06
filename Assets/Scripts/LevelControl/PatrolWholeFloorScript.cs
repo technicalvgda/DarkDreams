cf37feffe18c0159898b6c5a4d45d42807709637
topher8 <ctoph.1990@gmail.com>
2015-11-05 16:55:53 -0800

    modify chasing monster to patrol the entire length of the room it is in.
    I just commented out things related to patrol distance since that was
    being used to cap the distance in the ChasingMonster script. And I made
    a new script that used an OnTriggerExit2D function that checked if the
    monster was a ChasingMonster and then reference the FlipEnemy() method
    in the ChasingMonster script. The Chasing Monster needs a rigidbody with
    gravity set to zero but i'm not sure how to commit that. Sorry.

---
+++ Assets/Scripts/LevelControl/PatrolWholeFloorScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PatrolWholeFloorScript : MonoBehaviour {
+    //used to refrence the ChasingMonster script
+    public ChasingMonster chase;
+    //used for initilazation
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    //checks if the collider object is ChasingEnemy, is so flip it.
+    void OnTriggerExit2D(Collider2D col)
+    {
+        
+        if (col.gameObject.name == "ChasingEnemy")
+        {
+            chase = col.GetComponent<ChasingMonster>();
+            chase.FlipEnemy();
+        }
+    }
+}
