372190ebf2ab2fe9d2b295a76af0f1c09e9ffb2d
Darkchicken <joshshucker@hotmail.com>
2015-11-12 20:30:40 -0800

    Items now glow when on same floor
    Changed items to particle glow script
    Removed box collider for particle glow

---
+++ Assets/Scripts/LevelControl/ParticleGlow.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleGlow : MonoBehaviour {
+
+    ParticleSystem particle;
+    GameObject player;
+    Transform playerPos;
+
+    void Awake()
+    {
+        particle = GetComponent<ParticleSystem>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        playerPos = player.GetComponent<Transform>();
+        particle = this.GetComponent<ParticleSystem>();
+        //particle.emissionRate = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If this object is not on the same elevation as the player, do nothing
+        if (this.transform.position.y < playerPos.position.y - 10 ||
+            this.transform.position.y > playerPos.position.y + 10)
+        {
+           // particle.Stop();
+            //particle.Clear();
+            particle.emissionRate = 0;
+            particle.startLifetime = 0;
+        }
+        else
+        {
+           // particle.Play();
+            particle.emissionRate = 1;
+            particle.startLifetime = 1;
+        }
+    }
+/*
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            particle.emissionRate = 1;
+            particle.startLifetime = 1;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            particle.emissionRate = 0;
+            particle.startLifetime = 0;
+        }
+    }
+    */
+}
