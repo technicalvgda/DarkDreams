35615ff2d63bd5f3cbbd16ef155a4e696cd0cb77
Nesskittycat <Nessa@Nessas-MacBook-Pro.local>
2015-11-15 18:28:56 -0800

    Make the script readable.

--- Assets/Scripts/MapGenerate/NightmareTower.cs
+++ Assets/Scripts/MapGenerate/NightmareTower.cs
@@ -2,6 +2,10 @@
 using System.Collections;
 using System.Collections.Generic;
 
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
 public class NightmareTower : MonoBehaviour {
 	
 	public const int NUMBER_OF_DOOR_PER_FLOOR = 2;
@@ -15,135 +19,152 @@ public class NightmareTower : MonoBehaviour {
 	
 	public int noRoomsPerFloor = 4;
 	
-	
+	//To keep track of what level is on each floor
 	private int level = 0;
 	
+	//To change position of instantiated hallway
 	private Vector3 sizeOfPrefab = new Vector3(0,0,0);
 	
-	//create each hallway
-	private List<Transform> bottomHallway = new List<Transform>();
-	private List<Transform> topHallway = new List<Transform>();
-	private List<GameObject> hallToRemove = new List<GameObject> ();
-	
-	//Get the the door to trigger the generate method
-	private GameObject doorToConnect;
-	private Vector2 playerPosition;
-	
-	//Variables to check for generation
+	//Variables to check for generation and destroy
 	private bool canGenerate;
 	private bool generated = false;
 	
-	//Last door on the map.
-	private string lastDoorOnMap = "";
-	private GameObject doorToBeTriggered;
-
+	//List of hallways
+	private List<Transform> evenHallway = new List<Transform>();
+	private List<Transform> oddHallway = new List<Transform> ();
+	private List<Transform> hallToRemoveE = new List<Transform>();
+	private List<Transform> hallToRemoveO = new List<Transform>();
+	
+	
 	void Awake() {
 		
-		if (doorRoom.Length < 4) {
+		if (doorRoom.Length < noRoomsPerFloor) {
 			Debug.Log("Please make sure there are at least 4 door rooms");
 			return;
 		}
-		
 		canGenerate = false;
 		initializeRooms ();
 		
 	}
-		
+	
 	// Use this for initialization
 	void Start () {
 		instantiateRooms ();
-		InvokeRepeating ("generate", noRoomsPerFloor-1, noRoomsPerFloor-1);
-		
+		//InvokeRepeating ("generate", noRoomsPerFloor-1, noRoomsPerFloor-1);        
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (doorToBeTriggered != null) {
-			if (doorToBeTriggered.GetComponent<TeleportDoors> ().used && doorToBeTriggered) {
-				canGenerate = true;
-				doorToBeTriggered.GetComponent<TeleportDoors> ().used = false;
-			}
-		} 
-
-	}
-	
-	void generate() {
-		Debug.Log ("check if i can generate");
-		if (canGenerate) {
-			Debug.Log ("YES I CAN GENERATE");
-			canGenerate = false;
-			generated = true;
-			randomizeRooms();
-			while (doorRoom[0].name.Equals(lastDoorOnMap) && doorRoom[1].name.Equals(lastDoorOnMap)) {
-				randomizeRooms();
-			}
-
-			Invoke("destroyHallway", noRoomsPerFloor);
-			
-		}
-	}
-	
 	void instantiateRooms() {
-		for(int i = 0; i < noRoomsPerFloor; i++) {
-
-			Transform obj = (Transform)(Instantiate(bottomHallway[i], transform.position + sizeOfPrefab , transform.rotation));
-			sizeOfPrefab.x += lengthOfRoom;
-			obj.name = "New Room " + i;
-			hallToRemove.Add (obj.gameObject);
-			Debug.Log ("ROOM ADDED TO INSTANTIATEDHALL: " + obj.name);
-		}
 		
+		generateEven ();
+		generateOdd ();
+
+		foreach (GameObject gameObj in GameObject.FindObjectsOfType<GameObject>()) {
+			if (gameObj.name == "Door To Be Removed E" || gameObj.name == "Door To Be Removed O") {
+				Destroy (gameObj.GetComponent("TeleportDoors"));
+			}
+		}
+
+
+		string s1 = evenHallway [0].name;
+		string s2 = evenHallway[evenHallway.Count-1].name;
+		string s3 = oddHallway [0].name;
+		string s4 = oddHallway[oddHallway.Count-1].name;
+		
+		string[] doorFirstBot = s1.Split (' ');
+		string[] doorLastBot = s2.Split(' ');
+		string[] doorFirstTop = s3.Split (' ');
+		string[] doorLastTop = s4.Split (' ');
+		
+		//Set the bottom door to the first door of the next floor
+		//Transform bottomDoorLast = evenHallway[evenHallway.Count-1].Find("Door" + doorLastBot[1]).transform;
+		//bottomDoorLast.GetComponent<TeleportDoors>().exit = oddHallway[0].Find("Door" + doorFirstTop[1]).transform;
+		
+		Debug.Log ("doorLast, doorFirst: " + doorLastBot[1] + ", " + doorFirstTop[1]);
+
+		//Set Trigger door for generation
+		/*doorToBeTriggered = GameObject.Find ("Door" + doorNumber1[1]);
+        Debug.Log ("Door to be Triggered: " + doorToBeTriggered.name);
+        s1 = bottomHallway [0].name;
+        s2 = topHallway [topHallway.Count - 1].name;
+        doorNumber1 = s1.Split(' ');
+        doorNumber2 = s2.Split(' ');
+        Transform firstDoor = GameObject.Find("Door" + doorNumber1[1]).transform;
+        Destroy(firstDoor.GetComponent("TeleportDoors"));
+        
+        Transform lastDoor = GameObject.Find("Door" + doorNumber2[1]).transform;
+        lastDoor.GetComponent<TeleportDoors>().exit = GameObject.Find("Door" + doorNumber2[1]).transform;
+        
+        lastDoorOnMap = doorNumber2[1];
+        bottomHallway.Clear ();
+        topHallway.Clear ();
+        */
+	}
+	
+	
+	void generateEven() {
+		//Spawn Even
+		for(int i = 0; i < noRoomsPerFloor; i++) {
+			Transform obj = (Transform)(Instantiate(evenHallway[i], transform.position + sizeOfPrefab , transform.rotation));
+			sizeOfPrefab.x += lengthOfRoom;
+			obj.name = "New Room E " + i;
+			if (i == 0) {
+				obj.name = "Room to Remove E";
+				string s1 = evenHallway [0].name;
+				string[] doorFirstBot = s1.Split (' ');
+				Transform firstDoorTop = obj.Find ("Door" + doorFirstBot [1]).transform;
+				obj.Find ("Door" + doorFirstBot[1]).name = "Door To Be Removed E";
+				
+			}
+			if (i == noRoomsPerFloor-1) {
+				string s2 = evenHallway[evenHallway.Count-1].name;
+				string[] doorLastBot = s2.Split(' ');
+				Transform lastDoorBot = obj.Find ("Door" + doorLastBot[1]).transform;
+				obj.Find ("Door" + doorLastBot[1]).name = "Door To Be Connected E";  
+				
+			}
+			hallToRemoveE.Add (obj);
+		}
+		//Change level
 		level++;
 		sizeOfPrefab.x = 0;
 		
 		//Size/Gap between each level (height of rooms)
 		sizeOfPrefab.y += heightOfRoom;
 		sizeOfPrefab.y += level;
-		
+	}
+	
+	void generateOdd() {
+		//Spawn Odd
 		for (int i = 0; i < noRoomsPerFloor; i++) {
-			Instantiate(topHallway[i], transform.position + sizeOfPrefab , transform.rotation);
+			Transform obj = (Transform)(Instantiate(oddHallway[i], transform.position + sizeOfPrefab , transform.rotation));
 			sizeOfPrefab.x += lengthOfRoom;
-			Debug.Log (topHallway[i]);
+			obj.name = "New Room O " + i;
+			if (i == 0) {
+				obj.name = "Room to Remove O";
+				string s3 = oddHallway[0].name;
+				string[] doorFirstTop = s3.Split(' ');
+				Transform firstDoorTop = obj.Find ("Door" + doorFirstTop [1]).transform;
+				obj.Find ("Door" + doorFirstTop[1]).name = "Door To Be Removed O";  
+
+			}
+			if (i == noRoomsPerFloor-1) {
+				string s4 = oddHallway[oddHallway.Count-1].name;
+				string[] doorLastTop = s4.Split(' ');
+				Transform lastDoorTop = obj.Find ("Door" + doorLastTop [1]).transform;
+				obj.Find ("Door" + doorLastTop[1]).name = "Door To Be Connected O";  
+
+			}
+			hallToRemoveO.Add (obj);
 		}
 		
+		//Change level
 		level++;
 		sizeOfPrefab.x = 0;
-		//Size/Gap between each level (height of rooms
+		
+		//Size/Gap between each level (height of rooms)
 		sizeOfPrefab.y += heightOfRoom;
 		sizeOfPrefab.y += level;
-		
-		string s1 = bottomHallway[bottomHallway.Count-1].name;
-		string s2 = topHallway[0].name;
-		string[] doorNumber1 = s1.Split(' ');
-		string[] doorNumber2 = s2.Split(' ');
-		
-		Transform bottomDoor = GameObject.Find("Door" + doorNumber1[1]).transform;
-		bottomDoor.GetComponent<TeleportDoors>().exit = GameObject.Find("Door" + doorNumber2[1]).transform;
-		
-		
-		
-		Transform topDoor = GameObject.Find("Door" + doorNumber2[1]).transform;
-		topDoor.GetComponent<TeleportDoors>().exit = GameObject.Find("Door" + doorNumber2[1]).transform;
-		Destroy (topDoor.GetComponent ("TeleportDoors"));
-		
-		//Set Trigger door for generation
-		doorToBeTriggered = GameObject.Find ("Door" + doorNumber1[1]);
-		Debug.Log ("Door to be Triggered: " + doorToBeTriggered.name);
-		s1 = bottomHallway [0].name;
-		s2 = topHallway [topHallway.Count - 1].name;
-		doorNumber1 = s1.Split(' ');
-		doorNumber2 = s2.Split(' ');
-		Transform firstDoor = GameObject.Find("Door" + doorNumber1[1]).transform;
-		Destroy(firstDoor.GetComponent("TeleportDoors"));
-		
-		Transform lastDoor = GameObject.Find("Door" + doorNumber2[1]).transform;
-		lastDoor.GetComponent<TeleportDoors>().exit = GameObject.Find("Door" + doorNumber2[1]).transform;
-		
-		lastDoorOnMap = s2;
-		
-		bottomHallway.Clear ();
-		topHallway.Clear ();
 	}
+	
 	
 	void randomizeRooms() {
 		//shuffle doorRooms
@@ -164,52 +185,30 @@ public class NightmareTower : MonoBehaviour {
 			otherRoom[i] = otherRoom[idx];
 			otherRoom[idx] = tmp;
 		}
-		
-		
 	}
 	
 	void initializeRooms()
-	{
+	{    
 		randomizeRooms ();
 		
 		//r to keep track of the room used
 		int r = 0;
-		Debug.Log ("r : " + r);
-		
-		
-		
 		//set door room to be the first room on each floor
-		bottomHallway.Add (doorRoom [r]);
+		evenHallway.Add (doorRoom [r]);
 		r++;
-		topHallway.Add (doorRoom [r]);
+		oddHallway.Add (doorRoom [r]);
 		r++;
-		Debug.Log ("r after: " + r);
 		for (int i = 0; i < noRoomsPerFloor-NUMBER_OF_DOOR_PER_FLOOR; i++) {
 			int randomRoom = Random.Range (0, otherRoom.Length);
-			bottomHallway.Add (otherRoom [i]);
-			topHallway.Add (otherRoom [randomRoom]);
+			evenHallway.Add (otherRoom [i]);
+			oddHallway.Add (otherRoom [randomRoom]);
 		}
 		//set door room to be the last room on each floor
-		bottomHallway.Add (doorRoom [r]);
+		evenHallway.Add (doorRoom [r]);
 		r++;
-		topHallway.Add (doorRoom [r]);
+		oddHallway.Add (doorRoom [r]);
 		r++;
 	}
 	
-	void destroyHallway() {
-		if (generated) {
-			Debug.Log ("DESTROY HALLWAY!!!");
-			
-			for (int i = 0; i < 4; i++) {
-				//Transform destroyObject = GameObject.Find (instantiatedHall[i]+"(Clone)").transform;
-				//Debug.Log ("THIS IS TO BE DESTROYED: " + destroyObject.name);
-				Debug.Log ("This is going t obe destroyed: " + hallToRemove[i].name);
-				Destroy (hallToRemove[i]);
-
-			}
-			
-		}
-		
-	}
 	
 }
