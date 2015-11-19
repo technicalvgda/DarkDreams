759bc7aab81b88e1cf8a4bce204e8718410cf706
Darkchicken <joshshucker@hotmail.com>
2015-11-19 10:49:56 -0800

    Modification to nightmare tower
    Swapped main camera for prefab version
    added borders that follow the hallways up

---
+++ Assets/Scripts/LevelControl/NightmareBorder.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NightmareBorder : MonoBehaviour {
+
+    Vector3 wallPos;
+    GameObject player;
+    // Use this for initialization
+    void Start ()
+    {
+        wallPos = transform.position;
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        wallPos.y = player.transform.position.y;
+        transform.position = wallPos;
+	}
+}
