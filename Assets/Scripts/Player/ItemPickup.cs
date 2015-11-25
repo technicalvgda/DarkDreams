fefa9791b9dece0b9a6b628016d99d1e5f30474a
Darkchicken <joshshucker@hotmail.com>
2015-11-24 20:35:27 -0800

    Fixed item text pause

--- Assets/Scripts/Player/ItemPickup.cs
+++ Assets/Scripts/Player/ItemPickup.cs
@@ -37,6 +37,7 @@ public class ItemPickup : MonoBehaviour {
 	//public GameObject canvas;
 	public bool flash = false;
     public bool textActive = false;
+    private bool playerTouching = false;
 	public FadingDarkness fadingDarkness;
 	PlayerControl playerScript;
     PauseScript pause;
@@ -50,6 +51,7 @@ public class ItemPickup : MonoBehaviour {
     
     void Start()
     {
+        playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControl>();
         pause = Camera.main.GetComponent<PauseScript>();
         audioHandler = GameObject.Find("AudioHandler").GetComponent<AudioHandlerScript>();
         // player = GameObject.Find("Player").GetComponent<PlayerControl>();
@@ -58,10 +60,78 @@ public class ItemPickup : MonoBehaviour {
         clickPosition = new Vector2(0f, 0f);
         //cameraScript = Camera.main.GetComponent<CameraFollowScript>();
     }
-    void OnTriggerStay2D (Collider2D other) {
+    void Update()
+    {
+        //if player is overlapping item
+        if (playerTouching == true)
+        {
+            float xNegPosition = transform.position.x - clickOffsetX;
+            float xPosPosition = transform.position.x + clickOffsetX;
+            float yPosPosition = transform.position.y + clickOffsetY;
+            float yNegPosition = transform.position.y - clickOffsetY;
+
+            ///get position of click
+            clickPosition.x = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+            clickPosition.y = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
+
+            if (Input.GetKeyDown(KeyCode.Space) || ((yNegPosition < clickPosition.y && clickPosition.y < yPosPosition) &&
+                (xNegPosition < clickPosition.x && clickPosition.x < xPosPosition) && Input.GetMouseButtonDown(0)))
+            {
+               
+                fadingDarkness = playerScript.fadingDarknessScript;
+                if (textActive == true)
+                {
+                    Time.timeScale = 1;
+                    pause.busy = false;
+                    playerScript.AddPoints(itemAdd); //will add the amount in player script
+                    Destroy(gameObject); //destroys the object
+                }
+                else
+                {
+
+                    itemTextPanel.SetActive(true);
+                    pause.busy = true;
+                    flash = true;
+                    textActive = true;
+                    audioHandler.PlayVoice(dialogueToPlay);
+                    Time.timeScale = 0;
+
+                }
+                if (fadingDarkness == null)
+                {
+                    Debug.Log("not found");
+                }
+                else
+                {
+                    fadingDarkness.flash = true;
+                }
+            
 
 
-		playerScript = other.GetComponent<PlayerControl>();
+            }
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            playerTouching = true;
+        }
+        
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            playerTouching = false;
+        }
+    }
+    /*
+    void OnTriggerStay2D (Collider2D other)
+    {
+
+
+		//playerScript = other.GetComponent<PlayerControl>();
        
 		
 
@@ -77,6 +147,7 @@ public class ItemPickup : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Space) || ((yNegPosition < clickPosition.y && clickPosition.y < yPosPosition) &&
             (xNegPosition < clickPosition.x && clickPosition.x < xPosPosition) && Input.GetMouseButtonDown(0)))
         {
+          
             if (other.GetComponent<PlayerControl>() == null)
             {
                 return;
@@ -95,10 +166,12 @@ public class ItemPickup : MonoBehaviour {
 
                 itemTextPanel.SetActive(true);
                 pause.busy = true;
-                //Time.timeScale = 0;
                 flash = true;
                 textActive = true;
                 audioHandler.PlayVoice(dialogueToPlay);
+                textPause = true;
+                Time.timeScale = 0;
+                
             }
             
             
@@ -116,17 +189,8 @@ public class ItemPickup : MonoBehaviour {
             }
         }
         
-
-		
-
-       
-
-		//flashingCanvas = GameObject.Find("FlashingLightCanvas")();
-		//Instantiate(flashingCanvas);
-
-
-	
-
-
 	}
+    */
+    
+   
 }	
