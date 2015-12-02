4241b45d0993a764f4838dde7fb277b0ce7c937d
Darkchicken <joshshucker@hotmail.com>
2015-12-01 23:00:23 -0800

    Added wait time to loading screen

--- Assets/Scripts/LevelControl/LoadingScene.cs
+++ Assets/Scripts/LevelControl/LoadingScene.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class LoadingScene : MonoBehaviour {
 
     private AsyncOperation async = null;
+    private int waitTime = 10;
     // Use this for initialization
     void Start()
     {
@@ -14,6 +15,7 @@ public class LoadingScene : MonoBehaviour {
 
     private IEnumerator LoadLevel(string Level)
     {
+        yield return new WaitForSeconds(waitTime);
         Application.LoadLevelAsync(Level);
         yield return 0;
     }
