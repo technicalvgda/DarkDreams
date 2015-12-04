ade192ff09fde38ce6242a70e670f94d74861dad
Darkchicken <joshshucker@hotmail.com>
2015-12-03 16:54:50 -0800

    Fixed credits

--- Assets/Scripts/LevelControl/CreditsScript.cs
+++ Assets/Scripts/LevelControl/CreditsScript.cs
@@ -5,7 +5,7 @@ public class CreditsScript : MonoBehaviour
 {
 
     AudioHandlerScript audioHandler;
-    float timer =133;
+    float timer =250;
     public float speed = 5;
     void Start()
     {
